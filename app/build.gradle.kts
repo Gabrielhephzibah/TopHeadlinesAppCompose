d368013c281e375ce53dfaa21b26a4b22c6e43c8
gabrielhephzibah <gabrielhephzibah24@gmail.com>
2024-01-14 16:00:45 +0000

    Use navigation argument to pass data between screens

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
+    //implementation("androidx.compose.ui:ui:1.6.0-alpha03")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
--- app/src/main/java/com/example/topheadlinesappcompose/presentation/topHeadlinesDetails/TopHeadlinesDetailsScreen.kt
+++ app/src/main/java/com/example/topheadlinesappcompose/presentation/topHeadlinesDetails/TopHeadlinesDetailsScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -16,45 +18,68 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.example.topheadlinesappcompose.R
 import com.example.topheadlinesappcompose.domain.model.TopHeadlinesItem
 import com.example.topheadlinesappcompose.ui.theme.nunitoFontFamilyLight
 import com.example.topheadlinesappcompose.ui.theme.tieneFontFamily
+import java.util.Base64
 
 @Composable
-fun TopHeadlinesDetailsScreen() {
+fun TopHeadlinesDetailsScreen(navController:NavController,
+                              title: String?,
+                              description: String?,
+                              imageUrl: String?,
+                              content: String?) {
    // val dataIntent = intent
-    TopHeadLinesDetails()
+    TopHeadLinesDetails(title, description, imageUrl, content)
+    println("Zibah:::Data::$title $imageUrl")
+//    val backStackEntry = navController.previousBackStackEntry
+//    println("Zibah:: BACK $backStackEntry")
 
 }
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
-fun TopHeadLinesDetails(modifier: Modifier = Modifier) {
+fun TopHeadLinesDetails(title: String?,
+                        description: String?,
+                        imageUrl: String?,
+                        content: String?) {
+
+    val decodeContent = String(Base64.getUrlDecoder().decode(content))
+    //val getData  = navController.previousBackStackEntry?.arguments?.getParcelable("data")
+//    val data = remember {
+//        mutableStateOf(navController.previousBackStackEntry?.arguments?.getParcelable<TopHeadlinesItem>("data")!!)
+//    }
+   // println("Zibah:::Data::$getData")
+//    val getData  = navController.previousBackStackEntry?.savedStateHandle?.get<DestinationScreenArgument>(
+//        "detailArgument"
+//    )
    // val data = intent.getParcelableExtra<TopHeadlinesItem?>("data")
     //println("DATA:: $data")
     Column {
-//        GlideImage(
-//            model = data?.imageUrl,
-//            contentDescription = "background Image",
-//            contentScale = ContentScale.FillBounds,
-//            modifier = Modifier
-//                .height(200.dp)
-//                .fillMaxWidth()
-//
-//        )
-        Image(
-            painter = painterResource(
-                id = R.drawable.wall
-            ),
+        GlideImage(
+            model = imageUrl,
+            contentDescription = "background Image",
             contentScale = ContentScale.FillBounds,
-            contentDescription = "image",
             modifier = Modifier
                 .height(200.dp)
                 .fillMaxWidth()
+
         )
+//        Image(
+//            painter = painterResource(
+//                id = R.drawable.wall
+//            ),
+//            contentScale = ContentScale.FillBounds,
+//            contentDescription = "image",
+//            modifier = Modifier
+//                .height(200.dp)
+//                .fillMaxWidth()
+//        )
         Column(
             modifier =
             Modifier
@@ -62,7 +87,7 @@ fun TopHeadLinesDetails(modifier: Modifier = Modifier) {
                 .padding(horizontal = 20.dp)
         ) {
             Text(
-                text =  "Headlines Title",//data?.title.toString(),
+                text =  title.toString(),
                 fontSize = 20.sp,
                 fontFamily = FontFamily.SansSerif,
                 fontWeight = FontWeight.Bold,
@@ -70,12 +95,12 @@ fun TopHeadLinesDetails(modifier: Modifier = Modifier) {
             )
 
             Text(
-                text =  "Headlines Description",//data?.description.toString(),
+                text =  description.toString(),//data?.description.toString(),
                 fontSize = 15.sp,
                 fontStyle = FontStyle.Italic,
                 fontFamily = nunitoFontFamilyLight
             )
-            Text(text =  "Headlines Content", //data?.content.toString(),
+            Text(text =  decodeContent, //data?.content.toString(),
                 modifier = Modifier.padding(vertical = 15.dp),
                 fontSize = 15.sp,
                 fontFamily = tieneFontFamily
